c90668d07e5ac1270a180528ed142648ca574463
Paul Johnson <paul@pjcj.net>
2025-07-21 11:53:28 +0200

    Add test documentation

--- t/lib/ViolationFinder.pm
+++ t/lib/ViolationFinder.pm
@@ -54,3 +54,127 @@ sub bad ($policy, $code, $expected_message, $description) {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+ViolationFinder - Test utility for finding Perl::Critic policy violations
+
+=head1 SYNOPSIS
+
+  use ViolationFinder qw( find_violations count_violations good bad );
+
+  # Find all violations for a policy
+  my @violations = find_violations $policy, $code;
+
+  # Test that code has expected number of violations
+  count_violations $policy, $code, 3, "should find 3 violations";
+
+  # Test that code has no violations
+  good $policy, $code, "valid code should not violate policy";
+
+  # Test that code has exactly one violation with expected message
+  bad $policy, $code, "expected message", "invalid code should violate";
+
+=head1 DESCRIPTION
+
+ViolationFinder provides utility functions for testing Perl::Critic policies.
+It simplifies the process of checking whether code violates specific policies
+and validating the violation messages.
+
+=head1 FUNCTIONS
+
+=head2 find_violations
+
+  my @violations = find_violations $policy, $code;
+
+Finds all violations of a given Perl::Critic policy in the provided code.
+
+Parameters:
+
+=over 4
+
+=item * C<$policy> - A Perl::Critic policy object
+
+=item * C<$code> - Perl source code as a string
+
+=back
+
+Returns a list of violation objects.
+
+=head2 count_violations
+
+  my @violations = count_violations$policy, $code, $expected_violations, $desc;
+
+Tests that code has the expected number of violations for a given policy.
+
+Parameters:
+
+=over 4
+
+=item * C<$policy> - A Perl::Critic policy object
+
+=item * C<$code> - Perl source code as a string
+
+=item * C<$expected_violations> - Number of expected violations
+
+=item * C<$desc> - Test description for the assertion
+
+=back
+
+Returns the list of violations found.
+
+=head2 good
+
+  good $policy, $code, $description;
+
+Tests that code has no violations for a given policy.
+
+Parameters:
+
+=over 4
+
+=item * C<$policy> - A Perl::Critic policy object
+
+=item * C<$code> - Perl source code as a string
+
+=item * C<$description> - Test description for the assertion
+
+=back
+
+This is a convenience function that verifies the code passes the policy check.
+
+=head2 bad
+
+  bad $policy, $code, $expected_message, $description;
+
+Tests that code has exactly one violation with an expected message.
+
+Parameters:
+
+=over 4
+
+=item * C<$policy> - A Perl::Critic policy object
+
+=item * C<$code> - Perl source code as a string
+
+=item * C<$expected_message> - Expected text in the violation message
+
+=item * C<$description> - Test description for the assertion
+
+=back
+
+This function verifies that the code violates the policy exactly once and that
+the violation message contains the expected text.
+
+=head1 AUTHOR
+
+Paul Johnson, E<lt>paul@pjcj.netE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
